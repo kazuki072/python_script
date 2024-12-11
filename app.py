197fceee48c9ea17f0ad9ddc20bd2c933847752e
kazuki072 <miyamotokazuki0722@icloud.com>
2024-12-11 20:19:24 +0900

    変更

--- app.py
+++ app.py
@@ -3,19 +3,22 @@ from flask import render_template , redirect ,request,session
 from flask_login import UserMixin , LoginManager ,login_user , logout_user , login_required
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.types import PickleType
-from sqlalchemy.dialects.sqlite import JSON
-
+from sqlalchemy.types import JSON
+from sqlalchemy import inspect
+from sqlalchemy.orm import relationship
+from flask_migrate import Migrate
 import os
 from datetime import datetime
 from werkzeug.security import generate_password_hash , check_password_hash
 import pytz
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.urandom(24)
-db=SQLAlchemy(app)
 
-#単元list
-subjiects_c1_math=['正負の数','文字式の計算','方程式','方程式の利用','比例','比例とグラフ','反比例','平面図形',]
+db=SQLAlchemy(app)
+migrate = Migrate(app , db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -24,6 +27,18 @@ login_manager.init_app(app)
 def loader_user(user_id):
     return User.query.get(int(user_id))
 
+#単元list
+class Subjiect(db.Model):
+    id =db.Column(db.Integer,primary_key=True)
+    grade = db.Column(db.String(20),)
+    subjiect = db.Column(db.String(20))
+    topic = db.Column(db.String(50),nullable=False)
+    vote = db.Column(db.Integer,default=0)
+
+
+
+
+
 class Lesson(db.Model):
     id = db.Column(db.Integer , primary_key=True)
     subjiect = db.Column(db.String(50))
@@ -31,7 +46,7 @@ class Lesson(db.Model):
     time = db.Column(db.String(20))
     max_number = db.Column(db.Integer)
     students_count=db.Column(db.Integer)
-    member = db.Column(db.JSON)
+    member = db.Column(db.String(250))
     teacher = db.Column(db.String(20))
 
 class User(UserMixin , db.Model):
@@ -98,69 +113,137 @@ def logout():
 def home():
     return redirect('/login')
 
+
+def Data():
+    grade=request.form.get('grade')
+    subjiect=request.form.get('subjiect')
+    topic=request.form.get('topic')
+
+    query=Subjiect.query
+    if grade !='all':
+        query = query.filter_by(grade=grade)
+    if subjiect !='all':
+        query = query.filter_by(subjiect=subjiect)
+    if topic:
+        query = query.filter(Subjiect.topic.like(f'%{topic}%'))
+
+    results = query.all()
+
+    data={}
+    for item in results:
+        if item.grade not in data:
+            data[item.grade] = {}
+        if item.subjiect not in data[item.grade]:
+            data[item.grade][item.subjiect]=[]
+        data[item.grade][item.subjiect].append({'topic':item.topic,'id':item.id})
+    return data
+
+
+
 @app.route('/student',methods=['POST','GET'])
 @login_required
 def student():
     username=session.get('username')
+
     if request.method == 'POST':
         action=request.form.get('action')
+        lesson_id = request.form.get('lesson_id')
+        lessons=Lesson.query.all()
+
+        #探す関連
         if action=='search':
-            lessons=Lesson.query.all()
-            return render_template('student.html',lessons=lessons)
+            return render_template('student.html',lessons=lessons,username=username)
+        
+        #トレンド関連
         elif action=='trend':
-            return render_template('student_trend.html')
+            return render_template('student_trend.html',)
+        
+        #参加している授業関連
         elif action=='attend':
-            return render_template('student_attend.html')
+            return render_template('student_attend.html',lessons=lessons)
+        
+        #ユーザー情報関連
         elif action=='user_imformation':
             return render_template('student_user_imformation.html')
-        db.session.commit()
-        lesson_id = int(request.form.get('lesson_id'))
-        lesson = Lesson.query.get(lesson_id)
-        if (lesson.students_count < lesson.max_number) and (username not in lesson.member):
-            lesson.students_count +=1
-            update_member=lesson.member
-            update_member.append(username)
-            lesson.member = update_member
-            print(lesson.member)
-            try:
-                db.session.flush()
+        
+        #vote関連
+        elif action=='vote':
+            data=0
+            return render_template('student_vote.html',data=data)
+        
+        elif action=='look_for':
+            data=Data()
+            return render_template('student_vote.html',data=data)
+        
+        elif action=='vote_id':
+            data=Data()
+            vote_id = request.form.get('vote_id')
+            subjiect= Subjiect.query.get(vote_id)
+            subjiect.vote += 1
+            db.session.commit()
+            return render_template('student_vote.html',data=data)
+        
+        elif action==lesson:
+            lesson_id = request.form.get('lesson_id')
+            lesson = Lesson.query.get(lesson_id)
+            members = lesson.member.split(',') if lesson.member else []
+            if (lesson.students_count < lesson.max_number) and (username not in members):
+                lesson.students_count +=1
+                members.append(username)
+                lesson.member = ','.join(members)
                 db.session.commit()
-                print(lesson.member)
+                return redirect('/student')
 
-            except Exception as e:
-                print(e)
-            print(update_member)
-            lessons = Lesson.query.all()
-            for i in lessons:
-                print(i.id , i.member)
-            return redirect('/student')
-        elif username in lesson.member:
-            print('もう参加しています')
-        else:
-            print('満員です')
-
+            elif username in members:
+                return redirect('/student')
+            else:
+                print('満員です')
     else:
         lessons=Lesson.query.all()
-        return render_template('student.html',lessons=lessons)
+        return render_template('student.html',lessons=lessons,username=username)
 
 @app.route("/teacher",methods=['POST','GET'])
 @login_required
 def teacher():
+    username=session.get('username')
+    lessons = Lesson.query.all()
+    data=0
     if request.method == 'POST':
         action=request.form.get('action')
+
         if action=='create':
-            return render_template('teacher.html',subjiects_c1_math = subjiects_c1_math)
+            return render_template('teacher.html',data=data)
+        
         elif action=='trend':
             return render_template('teacher_trend.html')
+        
         elif action=='user_imformation':
             return render_template('teacher_user_imformation.html')
-        lesson=request.form.get('lesson')
-        print(f'received:{lesson}')
-        for i in subjiects_c1_math:
-            if lesson==i:
-                return redirect(f'/create_lesson/{i}')
+        
+        elif action=='created':
+            created=[]
+            for i in lessons:
+                if i.teacher == username:
+                    created.append(i)
+            return render_template('teacher_created.html',created=created)
+        
+        elif action=='lesson':
+            lesson_id = request.form.get('lesson_id')
+            lesson= Subjiect.query.get(lesson_id)
+            return redirect(f'/create_lesson/{lesson.topic}')
+        
+        elif action=='search':
+            data=Data()
+
+            return render_template('teacher.html',data=data)
+        else:
+            
+            return render_template('teacher.html',data=data)
+
     else:
-        return render_template('teacher.html',subjiects_c1_math = subjiects_c1_math)
+        return render_template('teacher.html',data=data)
+
+
 
 @app.route("/create_lesson/<lesson>",methods=['POST','GET'])
 @login_required
@@ -170,7 +253,7 @@ def craete_lesson(lesson):
         people=request.form.get('people')
         title=request.form.get('title')
         username=session.get('username')
-        lesson = Lesson(time=time , title=title ,max_number=people,students_count=0,subjiect=lesson,teacher=username,member=[])
+        lesson = Lesson(time=time , title=title ,max_number=people,students_count=0,subjiect=lesson,teacher=username,member='')
 
         db.session.add(lesson)
         db.session.commit()
@@ -179,3 +262,18 @@ def craete_lesson(lesson):
     else:    
         return render_template('create_lesson.html',lesson=lesson)
     
+
+@app.route("/topics/add",methods=['POST','GET'])
+def topics():
+    if request.method=='POST':
+        grade=request.form.get('grade')
+        subjiect=request.form.get('subjiect')
+        topic=request.form.get('topic').split(',') if request.form.get('topic') else []
+        if topic:
+            for i in topic:
+                add=Subjiect(grade=grade,subjiect=subjiect,topic=i)
+                db.session.add(add)
+                db.session.commit()
+        return render_template('topics_add.html')
+    else:
+        return render_template('topics_add.html')
